ca2068a30465ee5c02497a4f06e6b4f363b9b964
Dulanga Nimesha <idulanganimesha@gmail.com>
2021-06-01 16:46:33 +0530

    Third Commit

--- src/Controller/MainFormController.java
+++ src/Controller/MainFormController.java
@@ -1,4 +1,24 @@
 package Controller;
 
+import com.jfoenix.controls.JFXRippler;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class MainFormController {
+    public JFXRippler rplAddStudent;
+    public AnchorPane apeAddStudent;
+
+
+    public void initialize(){
+        rplAddStudent.setControl(apeAddStudent);
+        apeAddStudent.setFocusTraversable(true);
+    }
+
+    public void pneAddNew_OnKeyPress(KeyEvent keyEvent) {
+
+        if(keyEvent.getCode() == KeyCode.ENTER || keyEvent.getCode() == KeyCode.SPACE){
+            rplAddStudent.createManualRipple().run();
+        }
+    }
 }
